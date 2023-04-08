9e9f3a7b434bbe18f868127367f85ef86aa34d8c
Fazeel Jaffer <fazeeljaffer@gmail.com>
2023-04-08 23:20:24 +0530

    65gjffj

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import mysql.connector
 import re
+from PIL import Image
+from io import BytesIO
 app = Flask(__name__)
 app.secret_key = "fazubazu123"
-
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Fazeel@1234'
 app.config['MYSQL_DB'] = 'teacher'
-
 mysql = MySQL(app)
+
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
 
 @app.route('/')
 def allusers():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM faculty')
-    accounts=cursor.fetchall()
+    accounts = cursor.fetchall()
     return render_template('allusers.html', accounts=accounts)
+
+
 @app.route('/myflaskproject/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
@@ -47,15 +58,19 @@ def login():
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 # http://localhost:5000/python/logout - this will be the logout page
+
+
 @app.route('/myflaskproject/logout')
 def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 # http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+
+
 @app.route('/myflaskproject/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
@@ -66,11 +81,21 @@ def register():
         username = request.form['username'].upper()
         password = request.form['password']
         email = request.form['email']
-        department=request.form['department'].upper()
-        contact=request.form['contact']
-        image=request.files['image']
+        department = request.form['department'].upper()
+        contact = request.form['contact']
+        # Retrieve the image file from the form
+        img_file = request.files['image']
+        # Convert the image file to bytes
+        img_bytes = img_file.read()
+        # Convert the image bytes to a PIL image object
+        img = Image.open(BytesIO(img_bytes))
+        # Convert the PIL image to a bytes object
+        img_byte_arr = BytesIO()
+        img.save(img_byte_arr, format="PNG")
+        img_bytes = img_byte_arr.getvalue()
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM faculty WHERE username = %s', (username,))
+        cursor.execute(
+            'SELECT * FROM faculty WHERE username = %s', (username,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
@@ -83,7 +108,9 @@ def register():
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO faculty VALUES (NULL, %s, %s, %s,%s,%s,%s,%s)', (username, password, email,department,contact,image,'Present'))
+            sql = "INSERT INTO faculty (username, password, email, department, contact, image, status) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            data = (username, password, email, department, contact, img_bytes, 'Present')
+            cursor.execute(sql, data)
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
@@ -91,14 +118,24 @@ def register():
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
+# @app.route('/myflaskproject/home')
+# def home():
+#     # Check if user is loggedin
+#     if 'loggedin' in session:
+#         # User is loggedin show them the home page
+#         return render_template('allusers.html', username=session['username'])
+#     # User is not loggedin redirect to login page
+#     return redirect(url_for('login'))
+
+
 @app.route('/myflaskproject/home')
 def home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM faculty')
+    accounts = cursor.fetchall()
+    return render_template('allusers.html', accounts=accounts)
+
+
 @app.route('/myflaskproject/profile')
 def profile():
     # Check if user is loggedin
@@ -113,11 +150,14 @@ def profile():
     return redirect(url_for('login'))
 
 
-@app.route('/update_column',methods=['POST'])
+@app.route('/update_column', methods=['POST'])
 def update_column():
-        new_value = request.form.get('new_value')
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('UPDATE faculty SET status =%s WHERE id =%s',(new_value,session['id']))
-        mysql.connection.commit()
-        return "Status Changed Successfully"
-    
+    new_value = request.form.get('new_value')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('UPDATE faculty SET status =%s WHERE id =%s',
+                   (new_value, session['id']))
+    mysql.connection.commit()
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM faculty')
+    accounts = cursor.fetchall()
+    return render_template('allusers.html', accounts=accounts)
